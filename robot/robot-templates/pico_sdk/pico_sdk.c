d1571c9922b28bfd67fbea38358e5d2a377e848e
vladpaun1 <vlad.paun06@gmail.com>
2025-12-16 10:21:43 +0100

    blinking working

--- robot/robot-templates/pico_sdk/pico_sdk.c
+++ robot/robot-templates/pico_sdk/pico_sdk.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 
+#ifndef PICO_DEFAULT_LED_PIN
+#define PICO_DEFAULT_LED_PIN 25
+#endif
 
-int main()
-{
-    stdio_init_all();
+int main() {
+  stdio_init_all(); // optional, but fine to keep
+                    // :contentReference[oaicite:0]{index=0}
 
-    while (true) {
-        ;
-    }
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+  while (true) {
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
+    sleep_ms(250);
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+    sleep_ms(250);
+  }
 }
