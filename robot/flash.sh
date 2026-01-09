b3ef9802dba86f90d4c1e20d59ba9155100c58be
vladpaun1 <vlad.paun06@gmail.com>
2026-01-09 23:15:23 +0100

    finished assignemnt 1, organized repo

--- robot/flash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cmake --build build
-picotool load build/robot.uf2
-picotool reboot
---
+++ robot/src/motors_pwm.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum {
+  MOTOR_LEFT = 0,
+  MOTOR_RIGHT = 1,
+  MOTOR_BOTH = 2,
+} motor_select_t;
+
+typedef enum {
+  MOTOR_DIR_FORWARD = 0,
+  MOTOR_DIR_REVERSE = 1,
+} motor_dir_t;
+
+void motors_pwm_init(void);
+void motors_pwm_stop(motor_select_t which);
+void motors_pwm_drive(motor_select_t which, motor_dir_t dir, uint8_t duty_percent);
+void motors_pwm_drive_lr(motor_dir_t left_dir, uint8_t left_duty, motor_dir_t right_dir, uint8_t right_duty);
